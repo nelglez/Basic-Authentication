e3261732d22bc7b8fc745b6723278636ee7b4126
Nelson Gonzalez <37880972+nelglez@users.noreply.github.com>
2019-05-21 15:14:24 -0400

    Add files via upload

---
+++ AnimalSpotter-master/AnimalSpotter/View Controllers/AnimalDetailViewController.swift
@@ -0,0 +1,56 @@
+//
+//  AnimalDetailViewController.swift
+//  AnimalSpotter
+//
+//  Created by Ben Gohlke on 4/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AnimalDetailViewController: UIViewController {
+    
+    var apiController: APIController?
+    var animalName: String?
+    
+    @IBOutlet weak var timeSeenLabel: UILabel!
+    @IBOutlet weak var coordinatesLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var animalImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getDetails()
+    }
+    
+    func getDetails() {
+        guard let apiController = apiController,
+        let animalName = animalName else { return }
+        
+        apiController.fetchDetails(for: animalName) { (result) in
+            if let animal = try? result.get() {
+                DispatchQueue.main.async {
+                    self.updateViews(with: animal)
+                }
+                apiController.fetchImage(at: animal.imageURL, completion: { result in
+                    if let image = try? result.get() {
+                        DispatchQueue.main.async {
+                            self.animalImageView.image = image
+                        }
+                    }
+                })
+            }
+        }
+        
+    }
+    
+    func updateViews(with animal: Animal) {
+        title = animal.name
+        descriptionLabel.text = animal.description
+        coordinatesLabel.text = "lat: \(animal.latitude), long:\(animal.longitude)"
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .short
+        timeSeenLabel.text = df.string(from: animal.timeSeen)
+    }
+}
---
+++ AnimalSpotter-master/AnimalSpotter/View Controllers/AnimalsTableViewController.swift
@@ -0,0 +1,73 @@
+//
+//  AnimalsTableViewController.swift
+//  AnimalSpotter
+//
+//  Created by Ben Gohlke on 4/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AnimalsTableViewController: UITableViewController {
+    
+    private var animalNames: [String] = []
+    
+    let apiController = APIController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if apiController.bearer == nil {
+            performSegue(withIdentifier: "LoginViewModalSegue", sender: self)
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animalNames.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalCell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = animalNames[indexPath.row]
+
+        return cell
+    }
+
+    // MARK: - Actions
+    @IBAction func getAnimals(_ sender: UIBarButtonItem) {
+        apiController.fetchAllAnimalNames { result in
+            if let names = try? result.get() {
+                DispatchQueue.main.async {
+                    self.animalNames = names
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowAnimalDetailSegue",
+            let detailVC = segue.destination as? AnimalDetailViewController {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                detailVC.animalName = animalNames[indexPath.row]
+            }
+            detailVC.apiController = apiController
+        }
+        else if segue.identifier == "LoginViewModalSegue" {
+            if let loginVC = segue.destination as? LoginViewController {
+                loginVC.apiController = apiController
+            }
+        }
+    }
+}
